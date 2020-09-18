c8ded6f9f28cbacec5586714fdc5a3dfcb881e21
Felipe Vogel <fps.vogel@gmail.com>
2020-09-18 15:31:02 -0400

    post on Windows Terminal, improve navigation

--- .zs/header.lua
+++ .zs/header.lua
@@ -6,7 +6,15 @@ function prependHeader (filename, title)
     template = io.open(filename, "r")
     templateStr = template:read("*all")
     headerEnd = templateStr:find("\n\n")
-    templateStr = headerStr:gsub("<< from header.lua >>", title) .. templateStr:sub(headerEnd)
+    thisHeaderStr = ""
+    if filename:find("index") then
+        thisHeaderStr = headerStr:gsub("%.active\n\t+a%[href=\"/index.html\"%]", "")
+    elseif filename:find("about") then
+        thisHeaderStr = headerStr:gsub("%.active\n\t+a%[href=\"/about.html\"%]", "")
+    else
+        thisHeaderStr = headerStr
+    end
+    templateStr = thisHeaderStr:gsub("<< from header.lua >>", title) .. templateStr:sub(headerEnd)
     template = io.open(filename, "w+")
     template:write(templateStr)
     template:close()
